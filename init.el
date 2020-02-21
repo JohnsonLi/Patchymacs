d518d0d90ba26209428c914dc36195a51b00ec9b
Johnson Li <johnson.li61001@gmail.com>
2020-02-21 16:56:46 -0500

    removed thing

--- init.el
+++ init.el
@@ -30,17 +30,3 @@
 
 (load "~/.emacs.d/packages.el")
 (load "~/.emacs.d/config.el")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (company smex ido-vertical-mode all-the-icons avy dashboard lavender-theme auto-package-update use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
