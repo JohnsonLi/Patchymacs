60759bad1248221c08bd66b86a53624db6463c5e
Johnson Li <johnson.li61001@gmail.com>
2020-02-21 00:22:15 -0500

    packages

--- init.el
+++ init.el
@@ -35,9 +35,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (treemacs-evil lavender-theme use-package colorless-themes))))
+ '(package-selected-packages (quote (smex lavender-theme use-package colorless-themes))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- packages.el
+++ packages.el
@@ -1,3 +1,11 @@
+(use-package auto-package-update
+  :defer nil
+  :ensure t
+  :config
+  (setq auto-package-update-delete-old-versions t)
+  (setq auto-package-update-hide-results t)
+  (auto-package-update-maybe))
+
 (use-package lavender-theme
   :ensure t
   :defer t)
@@ -20,3 +28,31 @@
   (setq dashboard-show-shortcuts nil)
   (setq dashboard-set-init-info nil)
   (setq dashboard-set-footer nil))
+
+(use-package avy
+  :ensure t
+  :bind
+  ("M-s" . avy-goto-char))
+
+(use-package ido
+  :init
+  (ido-mode 1)
+  :config
+  (setq ido-enable-flex-matching nil)
+  (setq ido-create-new-buffer 'always)
+  (setq ido-everywhere t))
+
+(use-package ido-vertical-mode
+  :ensure t
+  :init
+  (ido-vertical-mode 1))
+; This enables arrow keys to select while in ido mode. If you want to
+; instead use the default Emacs keybindings, change it to
+; "'C-n-and-C-p-only"
+(setq ido-vertical-define-keys 'C-n-C-p-up-and-down)
+
+;(use-package smex
+;  :ensure t
+;  :init (smex-initialize)
+;  :bind
+;  ("M-x" . smex))
